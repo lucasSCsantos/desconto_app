511cd3ff7856aa8459e6b3205dee42d646c2b958
lucasSCsantos <luc.cristovam10@gmail.com>
2026-04-30 13:44:03 -0300

    Arquitetura limpa: Implementação do fluxo até camada de controllere chamada na main

--- src/app/use_cases/criar_pedido.py
+++ src/app/use_cases/criar_pedido.py
@@ -1,7 +1,11 @@
 from src.app.entities.pedido import Pedido
 from src.app.entities.desconto import DescontoNormal, DescontoVIP, DescontoPremium
+from src.app.gateways.pedido_gateway import IPedidoGateway
 
 class CriarPedido:
+		def __init__ (self, pedido_gateway: IPedidoGateway):
+				self.pedido_gateway = pedido_gateway
+
 		def executar(self, cliente: str, valor: float, tipo_desconto: str) -> Pedido:
 				if tipo_desconto.lower() == "normal":
 						desconto = DescontoNormal()
@@ -11,6 +15,12 @@ class CriarPedido:
 						desconto = DescontoPremium()
 				else:
 						raise ValueError("Tipo de desconto inválido, Use 'normal', 'vip' ou 'premium'.")
+				
+				pedido = Pedido(cliente=cliente, valor=valor, desconto=desconto)
+				self.pedido_gateway.salvar(pedido)
 			
-				return Pedido(cliente=cliente, valor=valor, desconto=desconto)	
+				return pedido
+
+		def listar_pedidos(self) -> list[Pedido]:
+				return self.pedido_gateway.listar()
 				
