beee3e40006d7f7623660f6c40a74e9dc3e9dd3a
lucasSCsantos <luc.cristovam10@gmail.com>
2026-05-14 14:01:55 -0300

    merge: juntar feature arquitetura limpa

--- src/app/adapters/controllers/pedido_controller.py
+++ src/app/adapters/controllers/pedido_controller.py
@@ -1,14 +1,20 @@
 from src.app.use_cases.criar_pedido import CriarPedido
 from src.app.dtos.criar_pedido_input_dto import CriarPedidoDTO
+from src.app.presenters.pedido_presenter import PedidoPresenter
 
 class PedidoController:
-		def __init__(self, criar_pedido_use_case: CriarPedido):
+		def __init__(self, criar_pedido_use_case: CriarPedido, presenter: PedidoPresenter):
 				self.criar_pedido_use_case = criar_pedido_use_case
+				self.presenter = presenter
 
 		def criar_pedido(self, cliente: str, valor: float, tipo_desconto: str):
 				input_dto = CriarPedidoDTO(cliente=cliente, valor_original=valor, tipo_desconto=tipo_desconto)
 
-				return self.criar_pedido_use_case.executar(input_dto)
+				output_dto = self.criar_pedido_use_case.executar(input_dto)
+
+				return self.presenter.apresentar(output_dto)
 
 		def listar_pedidos(self):
-				return self.criar_pedido_use_case.listar_pedidos()
+				output_dtos = self.criar_pedido_use_case.listar_pedidos()
+
+				return self.presenter.apresentar_lista(output_dtos)
