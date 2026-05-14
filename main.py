0a04fa423a63cc33097137fb2ddf92ff25c66b8f
lucasSCsantos <luc.cristovam10@gmail.com>
2026-05-14 14:10:33 -0300

    develop: fix na main

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from app.entities.desconto import DescontoNormal, DescontoVIP, DescontoPremium
 from app.entities.pedido import Pedido
 from app.use_cases.criar_pedido import CriarPedido
 from app.adapters.controllers.pedido_controller import PedidoController
+from app.presenters.pedido_presenter import PedidoPresenter
 
 def main() -> None:
 		database = MemoryDatabase()
-		repo = MemoryPedidoRepository(database=database)
-		criar_pedido_use_case = CriarPedido(repository=repo)
-		controller = PedidoController(criar_pedido_use_case=criar_pedido_use_case)
-		
+		pedido_gateway = MemoryPedidoRepository(database=database)
+		criar_pedido_use_case = CriarPedido(pedido_gateway=pedido_gateway)
+		presenter = PedidoPresenter()
+		controller = PedidoController(criar_pedido_use_case=criar_pedido_use_case, presenter=presenter)
+
 		pedido1 = controller.criar_pedido(cliente="Cliente A", valor=100.0, tipo_desconto="normal")
 		pedido2 = controller.criar_pedido(cliente="Cliente B", valor=200.0, tipo_desconto="vip")
 		pedido3 = controller.criar_pedido(cliente="Cliente C", valor=300.0, tipo_desconto="premium")
 
 		print("Pedidos Criados:")
-		print(f"Cliente: {pedido1.cliente}, Valor Original: {pedido1.valor_original}, Desconto: {pedido1.valor_desconto()}, Valor Final: {pedido1.valor_final(pedido1.valor_original)}")
-		print(f"Cliente: {pedido2.cliente}, Valor Original: {pedido2.valor_original}, Desconto: {pedido2.valor_desconto()}, Valor Final: {pedido2.valor_final(pedido2.valor_original)}")
-		print(f"Cliente: {pedido3.cliente}, Valor Original: {pedido3.valor_original}, Desconto: {pedido3.valor_desconto()}, Valor Final: {pedido3.valor_final(pedido3.valor_original)}")
+		print(pedido1)
+		print(pedido2)
+		print(pedido3)
 
+		print("\Pedidos Salvos:")
+		for pedido in controller.listar_pedidos():
+				print(pedido)
+				
 if __name__ == "__main__":
 		main()
